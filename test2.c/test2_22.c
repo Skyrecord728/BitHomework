e305804d2f0f43f39ed4ad5d57821bf3cbb83853
SkyRecord728 <Cky728211@gmail.com>
2025-02-22 21:04:15 +0800

    二分查找

---
+++ test2.c/test2_22.c
@@ -0,0 +1,60 @@
+
+//
+//int main() {
+//	int a = 20;
+//	int b = 40;
+//	int c;
+//	while (c = a % b) {
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d", b);
+//	return 0;
+//
+//}
+//int main() {
+//	int arr[] = { 1,2,3,4,5 };
+//	int sz = sizeof (arr) / sizeof (arr[0]);
+//	int i = 0;
+//	while (i <= sz) {
+//		printf("%c", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//int main() {
+//	int i = 1;
+//	int sum = 0;
+//	int ret = 1;
+//		for (i = 1; i <= 10; i++) 
+//		{
+//			ret = ret * i;
+//			sum = sum + ret;
+//		}
+//	printf("%d", sum);
+//	return 0;
+//}
+#include <stdio.h>
+int main() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int n = 1;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int right = sz - 1;
+	int left = 0;
+	while (left <= right) {
+	int mid = (left + right) / 2;
+		if (n > arr[mid]) {
+			left = mid + 1;
+		}
+		else if (n < arr[mid]) {
+			right = mid - 1;
+		}
+		else {
+			printf("找到了，下标为:%d\n", mid);
+			break;
+		}
+	}
+	if (left > right) {
+		printf("找不到");
+	}
+	return 0;
+}
